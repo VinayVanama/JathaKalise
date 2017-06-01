0d9df90a51cf1a9206e6618cd7d8ea7120be35d1
CYKUL App's <vinay@cykul.com>
2017-06-01 10:57:20 +0530

    Final Release

--- app/src/main/java/com/swathisai/jathakalise/AlbumView.java
+++ app/src/main/java/com/swathisai/jathakalise/AlbumView.java
@@ -35,7 +35,6 @@ public class AlbumView extends AppCompatActivity {
     ListView listImages;
     ProgressDialog pd;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
